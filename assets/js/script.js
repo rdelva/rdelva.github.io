ede83b85ab90fb652a512a0cfedb060e23dfb866
rdelva <roseldelva@gmail.com>
2020-05-22 19:20:14 -0400

    update site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 const credit = document.getElementById('credit');
 const header = document.getElementById('header');
 const menuButton = document.getElementById('menu-button');
-
-console.log(document.documentURI);
+const menu = document.getElementById('menu');
 
 if(header.classList.contains('header-homepage')){
     
 }
 
-console.log(document.pathname);
-//Add changes to page determined by id name
-if (document.url == 'index.html'){
-    console.log('Homepage');
-}
+
+
 
 
 // toggle menu button
 
-menuButton.addEventListener('click', event => {
+
+menuButton.addEventListener('click',  (e) => {
+
     
+   
+
 });
