601a378a737fc19ea716a888db49d89b4befeb25
rdelva <roseldelva@gmail.com>
2020-05-22 20:16:51 -0400

    setup mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,16 @@ if(header.classList.contains('header-homepage')){
 
 // toggle menu button
 
-//menu.style.display = 'none';  hide menu
+menu.style.display = 'none';  //hide menu
 menuButton.addEventListener('click',  (e) => {
 
+    if(menu.style.display === 'none'){
+        menu.style.display = 'block';
+        console.log('hi');
+
+    } else {
+        menu.style.display = 'none';
+    }
     
    
 
