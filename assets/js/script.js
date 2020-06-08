32c89a254753c7d088b096ec2b7a0731f24481a9
rdelva <roseldelva@gmail.com>
2020-06-08 16:26:24 -0400

    update website

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,15 @@ const menu = document.getElementById('menu');
 
 
 menuButton.addEventListener('click', (e) => {
-    if(menu.style.display == 'none'){
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
+  
+
+        if(menu.style.display == 'none'){
+            menu.style.display = 'block';
+        } else {
+            menu.style.display = 'none';
+        }
+
+
 });
 
 
@@ -30,38 +34,4 @@ document.addEventListener('scroll', (e) => {
    
 
 
-});
-/*
-console.log(window.onscroll);
-
-function stickyHeader(header){
-    //let sticky = header.offsetTop;
-    //console.log(sticky);
-
-    //if Scroll Y is more that 120 sticky
-
-  
-    let y = window.pageYOffset;
-    console.log(y);
-
-   // When the user scrolls the page, execute myFunction
-    // window.onscroll = function() {myFunction()};
-}
-*/
-
-
-
-// Get the offset position of the navbar
-
-
-/*Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-
-stickyHeader();*/
+}); 
